02d6166535810b9a3771bac373980d9bfb46ccf0
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2026-01-02 08:47:31 -0700

    Add option for user to add grid size

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 const sketchBox = document.querySelector("#grid-squares");
-let squareSize = 512/32;
-sketchBox.style.setProperty("--square-size", `${squareSize}px`);
+const selectedGridSize = document.querySelector(".grid-number");
+const buttonSelected = document.querySelector(".grid-enter");
+const buttonClear = document.querySelector(".clear-screen");
+console.log(selectedGridSize.value);
+buttonSelected.addEventListener('click', () => {
+    const inputGridSize = parseInt(selectedGridSize.value);
+    if (inputGridSize <1 || inputGridSize > 100) { alert("Please enter a number greater than 1 and less than 101");return;}
+    
+    while(sketchBox.firstChild) {
+        sketchBox.removeChild(sketchBox.firstChild);
+    }
+    console.log(inputGridSize);
+    createGrid(inputGridSize);
+}
+)
 
-for (i = 0; i < (32**2); i++) {
+// buttonClear.addEventListener('click'), () => {
+//     const pixelBoxClear = document.getElementsByClassName("drawn");
+//     while(pixelBoxClear.classList.contains("drawn")){
+//         pixelBoxClear.classList.remove("drawn");
+//     }
+// }    
+    
+
+function createGrid(size){
+    
+    let squareSize = 512/size;
+    sketchBox.style.setProperty("--square-size", `${squareSize}px`);
+    for (let i = 0; i < (size**2); i++) {
     const pixelBox = document.createElement("div");
     pixelBox.classList.add("square");
     sketchBox.appendChild(pixelBox);
 }
+}
+
+sketchBox.addEventListener('mouseover', (event) => {
+    if(!event.target.classList.contains("square")) return;
+    event.target.classList.add("drawn");
+})
