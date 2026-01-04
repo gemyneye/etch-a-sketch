4fcd4e3f2a88b134d2bfe6a2ad92ee0b5d66cd7d
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2026-01-03 18:47:42 -0700

    Add darkening after each pass by 10%

--- script.js
+++ script.js
@@ -9,9 +9,19 @@ function randomColor() {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
+// function increaseOpacity(){
+//     while (i <= 100) {
+//         console.log(`rgba(0, 0, 255, ${i})`);
+//         return `rgba(0, 0, 255, ${i})`;
+//         i += 10;
+//     }
+// }
+
 console.log(selectedGridSize.value);
 console.log(randomColor());
 
+
+//create grid size
 buttonSelected.addEventListener('click', () => {
     const inputGridSize = parseInt(selectedGridSize.value);
     if (inputGridSize <1 || inputGridSize > 100) { alert("Please enter a number greater than 1 and less than 101");return;}
@@ -19,13 +29,13 @@ buttonSelected.addEventListener('click', () => {
     while(sketchBox.firstChild) {
         sketchBox.removeChild(sketchBox.firstChild);
     }
-    console.log(inputGridSize);
     createGrid(inputGridSize);
 }
 );
 
 buttonClear.addEventListener('click', () => { 
-    const pixelBoxClear = sketchBox.getElementsByClassName("drawn"); while(pixelBoxClear.length > 0){ 
+    const pixelBoxClear = sketchBox.getElementsByClassName("drawn"); 
+    while(pixelBoxClear.length > 0){ 
         pixelBoxClear[0].classList.remove("drawn"); 
     } 
 } 
@@ -38,13 +48,24 @@ function createGrid(size){
     for (let i = 0; i < (size**2); i++) {
     const pixelBox = document.createElement("div");
     pixelBox.classList.add("square");
+    pixelBox.style.setProperty("--alpha", "0");
     sketchBox.appendChild(pixelBox);
 }
 }
 
 sketchBox.addEventListener('mouseover', (event) => {
     if(!event.target.classList.contains("square")) return;
-    if(event.target.classList.contains("drawn")) return;
-    event.target.style.setProperty("--ink", randomColor());
+    // if(event.target.classList.contains("drawn")) return;
+    // event.target.style.setProperty("--ink", randomColor());
+    let drawnSquare = parseFloat(window.getComputedStyle(event.target).getPropertyValue("--alpha"));
+    console.log(drawnSquare);
+    if(drawnSquare < 1) {
+        drawnSquare += .1;
+        console.log(drawnSquare);
+        event.target.style.setProperty("--alpha", drawnSquare);
+    } else {
+        return;
+    }
+    console.log(drawnSquare);
     event.target.classList.add("drawn");
 })
